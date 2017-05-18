6ed8313b1ddeadfb6993392a838618a2c1e7dd22
Karolina Kafel <karolina.rusin@gmail.com>
2017-05-18 16:19:43 +0200

    Solution to MigratoryBirds - HackerRank - E

---
+++ src/main/java/com/hackerrank/MigratoryBirds.java
@@ -0,0 +1,27 @@
+package com.hackerrank;
+
+import java.util.Scanner;
+
+public class MigratoryBirds {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] types = new int[5];
+        for (int i = 0; i < n; i++) {
+            int type = in.nextInt();
+            types[type-1]++;
+        }
+
+        int max = 0;
+        int answer = 0;
+        for (int i = 0; i < 5; i++) {
+            if (types[i] > max) {
+                max = types[i];
+                answer = i+1;
+            }
+        }
+        System.out.println(answer);
+    }
+
+}
