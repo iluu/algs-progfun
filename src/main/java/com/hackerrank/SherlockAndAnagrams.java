b9ddd05f971e768e71fb7ec6dc557620fc274972
Karolina Kafel <karolina.rusin@gmail.com>
2017-06-15 14:55:06 +0200

    SherlockAndAnagrams - HR - M

---
+++ src/main/java/com/hackerrank/SherlockAndAnagrams.java
@@ -0,0 +1,49 @@
+package com.hackerrank;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SherlockAndAnagrams {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            String input = in.next();
+            System.out.println(countAnagrams(input));
+        }
+    }
+
+    static int countAnagrams(String input) {
+        int result = 0;
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = i + 1; j < input.length(); j++) {
+                result += count(input.substring(i, j), input.substring(i + 1, input.length()));
+            }
+        }
+        return result;
+    }
+
+    private static int count(String value, String rest) {
+        int count = 0;
+        int[] pattern = pattern(value);
+        for (int i = 0; i <= rest.length() - value.length(); i++) {
+            String substring = rest.substring(i, i + value.length());
+            if (Arrays.equals(pattern, pattern(substring))) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static int[] pattern(String value) {
+        int[] pattern = new int[26];
+        for (Character c : value.toCharArray()) {
+            pattern[c - 'a']++;
+        }
+        return pattern;
+    }
+
+
+}
---
+++ src/test/java/com/hackerrank/SherlockAndAnagramsTest.java
@@ -0,0 +1,25 @@
+package com.hackerrank;
+
+import org.junit.Test;
+
+import static com.hackerrank.SherlockAndAnagrams.countAnagrams;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class SherlockAndAnagramsTest {
+
+    @Test
+    public void noAnagrams() {
+        assertThat(countAnagrams("abcd")).isEqualTo(0);
+    }
+
+    @Test
+    public void simpleAnagrams() {
+        assertThat(countAnagrams("abba")).isEqualTo(4);
+    }
+
+    @Test
+    public void simpleAnagrams1() {
+        assertThat(countAnagrams("ifailuhkqq")).isEqualTo(3);
+
+    }
+}
